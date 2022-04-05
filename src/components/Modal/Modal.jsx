7e86bba7270c29482458411bd36d40cbfa8e2407
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-04-05 15:26:01 +0300

    update modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,72 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import s from "./Modal.module.css";
 
-class Modal extends Component {
-  onCloseModalByEsc = (e) => {
+function Modal({ handleTogleModal, modalImg, tag }) {
+  const onCloseModalByEsc = (e) => {
     if (e.keyCode === 27) {
-      this.props.handleTogleModal();
+      handleTogleModal();
     }
   };
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.onCloseModalByEsc);
-  }
+  useEffect(() => {
+    window.addEventListener("keydown", onCloseModalByEsc);
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.onCloseModalByEsc);
-  }
+    return () => {
+      window.removeEventListener("keydown", onCloseModalByEsc);
+    };
+  });
 
-  render() {
-    const { modalImg, handleTogleModal, tag } = this.props;
-    console.log(modalImg);
-    return (
-      <div
-        className={s.Overlay}
-        onClick={(e) => {
-          if (e.target === e.currentTarget) {
-            handleTogleModal();
-          }
-        }}
-      >
-        <div className={s.Modal}>
-          <img src={modalImg} alt={tag} />
-        </div>
+  return (
+    <div
+      className={s.Overlay}
+      onClick={(e) => {
+        if (e.target === e.currentTarget) {
+          handleTogleModal();
+        }
+      }}
+    >
+      <div className={s.Modal}>
+        <img src={modalImg} alt={tag} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// class Modal extends Component {
+//   onCloseModalByEsc = (e) => {
+//     if (e.keyCode === 27) {
+//       this.props.handleTogleModal();
+//     }
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.onCloseModalByEsc);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.onCloseModalByEsc);
+//   }
+
+//   render() {
+//     const { modalImg, handleTogleModal, tag } = this.props;
+//     console.log(modalImg);
+//     return (
+//       <div
+//         className={s.Overlay}
+//         onClick={(e) => {
+//           if (e.target === e.currentTarget) {
+//             handleTogleModal();
+//           }
+//         }}
+//       >
+//         <div className={s.Modal}>
+//           <img src={modalImg} alt={tag} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   modalImg: PropTypes.string.isRequired,
